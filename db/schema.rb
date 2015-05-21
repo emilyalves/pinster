cedc835cc1270bda630329a54eaea78f3f46305e
Emily Alves <emilyleite11@gmail.com>
2015-05-20 20:22:14 -0700

    add pins/users association

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150520224211) do
+ActiveRecord::Schema.define(version: 20150520235711) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
